a29775419340ec5887b178d1c066c6dc26c82687
Rufaisashah <rufaisa.ashraf@gmail.com>
2026-03-18 13:37:22 +0100

    add audio files, state counters and confetti generator

--- courses/frontend/advanced-javascript/week_2/script.js
+++ courses/frontend/advanced-javascript/week_2/script.js
@@ -8,3 +8,19 @@ const resetBtn = document.getElementById('reset-btn');
 const actionOverlay = document.getElementById('action-overlay');
 const leaderboardList = document.getElementById('leaderboard-list');
 const clearBtn = document.getElementById('clear-btn');
+
+
+const startSound = new Audio('https://actions.google.com/sounds/v1/alarms/beep_short.ogg');
+const clickSound = new Audio('https://actions.google.com/sounds/v1/cartoon/pop.ogg');
+const winSound = new Audio('https://actions.google.com/sounds/v1/human_voices/crowd_cheer.ogg');
+
+// 3. State
+let sCount = 0;
+let lCount = 0;
+let isGameRunning = false;
+let countdownInterval;
+
+// 4. Slow-Motion Confetti Config
+const slowConfetti = { max: 80, size: 1.5, clock: 7 };
+const confettiS = new ConfettiGenerator({ target: 'confetti-s', ...slowConfetti });
+const confettiL = new ConfettiGenerator({ target: 'confetti-l', ...slowConfetti });
