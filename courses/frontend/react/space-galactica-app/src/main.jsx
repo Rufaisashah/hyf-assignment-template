c3087d6a182f65522c11425f3bf073acbc32831e
Rufaisashah <rufaisa.ashraf@gmail.com>
2026-05-05 16:20:56 +0200

    remove task details

--- courses/frontend/react/space-galactica-app/src/main.jsx
+++ courses/frontend/react/space-galactica-app/src/main.jsx
@@ -10,18 +10,6 @@ import { WishlistProvider } from "./contexts/WishlistContext.jsx";
 
 import "./main.css";
 
-// 🧑🏽‍🚀 Task - Week 4 - part 1
-// Create a Wishlist context to share the wishlist data across components.
-// There is a /src/contexts folder- you will have to create a new file there.
-// You will have to import and use the context provider here, so that all routes can access the context.
-// The context should have the following properties:
-// - planetsWishlist: an array of objects, each with a name and a thumbnail property.
-// - addPlanetToWishlist: a function that adds an item to the wishlist.
-// - removePlanetFromWishlist: a function that removes an item from the wishlist.
-// - isPlanetInWishlist: a function that checks if an item is in the wishlist.
-// - wishlistCount: a number that represents the number of items in the wishlist.
-// ❗ NOTE: you have already created some of the above in other components. Copy those implementations into the context and adjust them if needed.
-
 const router = createBrowserRouter([
   {
     element: <Layout />,
@@ -48,8 +36,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <WishlistProvider> 
-    <RouterProvider router={router} />
-     </WishlistProvider> 
+    <WishlistProvider>
+      <RouterProvider router={router} />
+    </WishlistProvider>
   </React.StrictMode>,
 );
